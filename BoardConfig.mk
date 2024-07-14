7df4baf4115ca5e28ef316ed28c9c554e68e86f7
bhcavusoglu <145139414+bhcavusoglu@users.noreply.github.com>
2024-07-14 20:00:26 +0300

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,178 @@
+#
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/corot
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
+# Power
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Assertation
+TARGET_OTA_ASSERT_DEVICE := corot
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6985
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Build Hack
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Kernel
+TARGET_KERNEL_ARCH := arm64
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_HEADER_ARCH := arm64
+
+TARGET_NO_KERNEL := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+
+BOARD_KERNEL_BASE := 0x3FFF8000
+BOARD_PAGE_SIZE := 4096
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x26F08000
+BOARD_TAGS_OFFSET := 0x07C88000
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_DTB_SIZE := 327372
+BOARD_DTB_OFFSET := 0x07C88000
+BOARD_HEADER_SIZE := 2128
+BOARD_VENDOR_CMDLINE := bootopt=64S3,32N2,64N2
+
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(BOARD_VENDOR_CMDLINE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE) --board ""
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
+
+BOARD_SUPER_PARTITION_SIZE := 9653190656
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_PARTITION_LIST := odm odm_dlkm product system system_ext vendor vendor_dlkm
+BOARD_MAIN_SIZE := 9648996352 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB
+
+BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_MAIN_PARTITION_LIST))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_USES_VENDOR_DLKMIMAGE := true
+
+# Hardware
+BOARD_USES_MTK_HARDWARE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6985
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 2712
+TARGET_SCREEN_WIDTH := 1220
+
+# Recovery
+TARGET_NO_RECOVERY := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
+# Screen density
+# TARGET_SCREEN_DENSITY := 480
+
+# Treble
+BOARD_VNDK_VERSION := current
+
+# Vendor_boot recovery ramdisk
+BOARD_USES_RECOVERY_AS_BOOT := 
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := 
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := 
+TW_LOAD_VENDOR_BOOT_MODULES := true
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_corot
+TARGET_RECOVERY_DEVICE_MODULES := libinit_corot
+
+# TWRP Configurations
+TW_FRAMERATE := 60
+TW_STATUS_ICONS_ALIGN := center
+TW_Y_OFFSET := 87
+TW_H_OFFSET := -87
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 1000
+TW_MAX_BRIGHTNESS := 4000
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+# Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Tools
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_LIBRESETPROP := true
+TW_INCLUDE_LPDUMP := true
+TW_INCLUDE_LPTOOLS := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_APEX := true
+TW_INCLUDE_NTFS_3G := true
+TARGET_USES_MKE2FS := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := tr_TR
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_DEVICE_VERSION := Xiaomi 13T Pro | Hakan
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone80/temp"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
+TW_BATTERY_SYSFS_WAIT_SECONDS := 6
+TW_BACKUP_EXCLUSIONS := /data/fonts
+TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
---
+++ README.md
@@ -0,0 +1,24 @@
+# TWRP device tree for Xiaomi 13T Pro
+Xiaomi 13T Pro (codenamed _"corot"_) 
+![corot](https://i02.appmifile.com/329_operator_sg/14/08/2023/3fcfdc0e83081349795e70056be19e35.png)
+## Device specifications
+
+|                   Basic | Spec Sheet                                                            |
+|------------------------:|:----------------------------------------------------------------------|
+|                     SoC | MediaTek Dimensity 9200+                                              |
+|                     CPU | 1x 3.35 GHz Cortex-X3, 3x 3.0 GHz Cortex-A715, 4x 2.0 GHz Cortex-A510 |
+|                     GPU | Immortalis-G715 MC11                                                  |
+|                  Memory | 12/16/24 GB                                                           |
+| Shipped Android Version | 13                                                                    |
+|                 Storage | 256/512/1024 GB                                                       |
+|                 Battery | 5000 mAh, Li-Polymer                                                  |
+
+## Features
+
+Works:
+- [X] ADB
+- [X] Display
+- [X] Touch 
+- [X] Fasbootd
+- [X] USB OTG
+
